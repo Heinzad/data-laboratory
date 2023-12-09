e6875ad1d47467347a88db860ad0f4d150513b1c
Ad Heinz <Heinzad@users.noreply.github.com>
2023-12-10 10:11:35 +1300

    flasky localization

--- flasky/README.md
+++ flasky/README.md
@@ -4,3 +4,45 @@ Flasky
 
 "flasky" is a simple flask application from the instructions in Miguel Grinberg (2018), [Flask Web Development](http://www.flaskbook.com). 
 Once working, it will get refactored into the more complex application, "alchemy".
+
+
+## Virtual Environment
+
+Activate the virtual environment in PowerShell by running: 
+
+```
+cd flasky
+
+flasky\Scripts\Activate.ps1
+```
+
+To succeed, this may first require a change to execution policies in Power Shell: 
+
+```
+Set-ExecutionPolicy Unrestricted -Scope Process
+``` 
+
+Success is visible when the path begins with (flasky) 
+
+
+# Requirements 
+
+Requirements can be updated at any time by running: 
+```
+pip freeze >requirements.txt
+```
+
+
+# Web Server 
+
+In VS Code powershell: 
+```
+$env:FLASK_APP = "hello.py"
+$env:FLASK_DEBUG = "1"
+```
+then: 
+```
+python -m flask run
+```
+
+the flask web server can be found at: http://localhost:5000/
--- flasky/hello.py
+++ flasky/hello.py
@@ -1,16 +1,42 @@
-from flask import Flask, render_template
+# app - packages
+from flask import Flask 
+
+# Templating -- packages
+from flask import render_template
 from flask_bootstrap import Bootstrap
 
+# Localization -- packages
+from flask_moment import Moment 
+from datetime import datetime 
+
+
+
+# app -- initialize
 app = Flask(__name__)
 
+# Templating -- initialise 
 bootstrap = Bootstrap(app)
 
+# Localization -- intialize 
+moment = Moment(app)
 
+
+# Templating -- routes
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html',
+                           current_time=datetime.utcnow()
+                           )
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+# Templating -- error handling
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 500
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
