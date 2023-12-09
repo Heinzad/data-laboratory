35eb2f57e6b99599f55e1582f99a970474bda22e
Ad Heinz <Heinzad@users.noreply.github.com>
2023-12-10 11:28:56 +1300

    flasky web forms

--- flasky/hello.py
+++ flasky/hello.py
@@ -9,10 +9,22 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment 
 from datetime import datetime 
 
+# Forms -- packages
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SubmitField 
+from wtforms.validators import DataRequired 
+
+# redirects & user sessions -- packages
+from flask import session, redirect, url_for 
+
+import os 
+
 
 
 # app -- initialize
-app = Flask(__name__)
+app = Flask(__name__) 
+# forms -- configuration
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 # Templating -- initialise 
 bootstrap = Bootstrap(app)
@@ -21,16 +33,19 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-# Templating -- routes
-@app.route('/')
-def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow()
-                           )
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+# Templating -- routes 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
 
 # Templating -- error handling
 @app.errorhandler(404)
@@ -40,3 +55,9 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+# Forms 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
